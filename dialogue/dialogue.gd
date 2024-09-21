c1f002b877f87cc8899ef9ad09ff5d6f738ca673
bf-bonkus <bf.bonkus@proton.me>
2024-09-21 17:06:25 -0600

    more dialogue

--- dialogue/dialogue.gd
+++ dialogue/dialogue.gd
@@ -1,6 +1,11 @@
 class_name Dialogue
 extends Node
 
+signal dialogue_started
+
+func start_dialogue():
+	dialogue_started.emit()
+
 class DialogueObject:
 	var text = ["yappuchino"]
 	var options: Array[Dictionary]
