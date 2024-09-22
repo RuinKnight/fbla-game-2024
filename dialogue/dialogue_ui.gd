6724d3f8b1cbd3092fe5ad6699d4be352c8c81c2
bf-bonkus <bf.bonkus@proton.me>
2024-09-22 15:05:18 -0600

    Work on Dialogue UI

--- dialogue/dialogue_ui.gd
+++ dialogue/dialogue_ui.gd
@@ -5,4 +5,4 @@ func _ready() -> void:
 	Dialogue.set_dialogue.connect(_on_dialogue_set)
 
 func _on_dialogue_set(dialogue_object, start_text):
-	$TextBox/Label.text = dialogue_object.text[start_text]
+	$Panel/Speech.text = dialogue_object.text[start_text]
