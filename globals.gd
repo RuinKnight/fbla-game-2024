7cebc9a1fbf622e59ef83313d1577ebfe4851d9b
bf-bonkus <bf.bonkus@proton.me>
2024-10-19 21:33:13 -0600

    update phantom camera

--- globals.gd
+++ globals.gd
@@ -2,7 +2,6 @@ extends Node
 
 signal set_dialogue(dialogue_object: Dialogue.DialogueObject, start_text: int)
 
-signal camera_target_set(target)
 
 func start_dialogue(dialogue_object: Dialogue.DialogueObject, start_text: int):
 	set_dialogue.emit(dialogue_object, start_text)
